a9581b0cd927c00ca8f43116c0338b08504a2aa9
karmanya17 <53150531+karmanya17@users.noreply.github.com>
2021-06-23 23:28:40 +0530

    Leetcode-Array
    Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
    
    If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
    
    The replacement must be in place and use only constant extra memory.

---
+++ Next Submission.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int i,l;
+        for(i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                break;
+            }
+        }
+        if(i<0)
+        {
+            reverse(nums.begin(),nums.end());
+        }
+        else
+        {
+           for(l=n-1;l>i;l--)
+        {
+            if(nums[l]>nums[i])
+            {
+                swap(nums[l],nums[i]);
+                break;
+
+            }
+        }
+
+      reverse(nums.begin()+i+1,nums.end());
+        }
+
+    }
+};
